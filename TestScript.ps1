6b9b3ad456e39e73e76a37c78c63dbb34c3de1f9
Lane <dwl1003@sru.edu>
2019-11-23 14:24:33 -0500

    deleted:    DeleteSMB1.txt 	deleted:    TestScript.ps1

--- TestScript.ps1
+++
@@ -1,9 +0,0 @@
-$TxtPath = "C:\Users\baseb\OneDrive\Documents\PS_Scripts\ComputerTest.csv"
-$File = Import-Csv $TxtPath
-$Computers = $File.Computers
-$x = 0
-foreach($Computer in $Computers)
-{
-    $x++
-}
-echo $x
